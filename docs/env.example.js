0e0006049298e81d88d39a4b2ce5e42fb14de48d
Bannaga Altieb Abdul Muhsin <bannaga.altieb@aces-co.com>
2025-11-25 22:29:25 +0300

    Clear repository

--- docs/env.example.js
+++
@@ -1,5 +0,0 @@
-window.__env = Object.assign(window.__env || {}, {
-  VITE_SUPABASE_URL: "https://your-project.supabase.co",
-  VITE_SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
-  VITE_PUSH_NOTIFICATIONS_ENABLED: "false",
-});
